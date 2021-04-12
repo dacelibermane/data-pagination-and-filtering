e26ceaf2ef85a0e9edda6b86472b8ea6a6482bbb
Dace Libermane <dace.libermane@googlemail.com>
2021-04-12 17:38:32 +0300

    add function that displays a page of 9 students

--- js/script.js
+++ js/script.js
@@ -13,10 +13,49 @@ For assistance:
 
 
 
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+//-----------------------------Display a page of 9 students-----------------
+
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list')
+const itemsPerPage = 9;
+
+//izsaucot funkciju list vietā ir data(jo tur glabājās viss info par studentiem)
+//page -> patreizēja lapa
+function showPage(list, currPage){
+   const startIndex = (currPage * itemsPerPage) - itemsPerPage;
+   const endIndex =currPage * itemsPerPage;
+   studentList.innerHTML = '';
+
+   for(let i = 0; i < list.length; i++){
+      if(i >= startIndex && i < endIndex){
+         const studentInfo =  `
+         <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>
+         `
+         studentList.insertAdjacentHTML('beforeend', studentInfo);
+      }
+   }
+}
+
+//-------------------------Pagination Buttons
+// function addPaginationButton(list) {
+//    const paginationBtns = list.length
+// }
+
+showPage(data, 1);
 
 
 
